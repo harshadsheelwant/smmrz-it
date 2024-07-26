3e5b1f4e31b7156102625d8aeabc095a80c4c323
Harshad Sheelwant <105483319+harshadsheelwant@users.noreply.github.com>
2024-07-26 18:40:41 +0530

    Fixed PDF summarization bugs

--- app.py
+++ app.py
@@ -63,16 +63,10 @@ def llm_pipeline_web(extracted_text):
 
 
 @st.cache_data
-#function to display the PDF of a given file
 def displayPDF(file):
-    # Opening file from file path
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
-
-    # Displaying File
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 #streamlit code
