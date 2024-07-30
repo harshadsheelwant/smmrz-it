6117bd094bc5e6baa8a2866c8daf58a79a9068b6
Harshad Sheelwant <105483319+harshadsheelwant@users.noreply.github.com>
2024-07-30 20:05:13 +0530

    Fixed bugs

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ def get_transcript(yt_url):
     except Exception:
         try:
             generated_transcripts = [trans for trans in transcript_list if trans.is_generated]
-            transcript = transcript_list.find_generated_transcript(['en-US'])
-            #transcript = generated_transcripts[0]
+            transcript = transcript_list.find_generated_transcript(['en'])
+            transcript = generated_transcripts['en']
         except Exception as e:
             st.error(f"No suitable transcript found: {e}")
             return ""
