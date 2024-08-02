b036cdccacef4b9a5863f649d8ef120b6bd0c921
Harshad Sheelwant <105483319+harshadsheelwant@users.noreply.github.com>
2024-08-02 19:23:36 +0530

    Fixed bugs

--- app.py
+++ app.py
@@ -89,10 +89,10 @@ def displayPDF(file):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
 
     # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    st.markdown(pdf_display, unsafe_allow_html=False)
 
 #streamlit code
 st.set_page_config(
